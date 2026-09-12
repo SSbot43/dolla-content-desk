a841e02a80dd0f473e0d3058dcac952cf2a8fa4a
SSbot43 <mails@megacodes.in>
2026-09-12 15:15:37 +0530

    Enable automatic internal links in Dolla Content Desk

--- bulk_launcher.py
+++ bulk_launcher.py
@@ -24,12 +24,14 @@ from manual_override import install as install_manual_override
 from review_state_fix import install as install_review_state_fix
 from encoding_git import install as install_encoding_git
 from resilient_git import install as install_resilient_git
+from dolla_internal_links import install as install_internal_links
 
 install_publish_policy(desk)
 install_manual_override(desk)
 install_review_state_fix(desk)
 install_encoding_git(desk)
 install_resilient_git(desk)
+install_internal_links(desk)
 app = desk.app
 
 SYNC_SECONDS = int(os.environ.get("CONTENT_SYNC_SECONDS", "45"))
